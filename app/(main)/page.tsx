4cf76c58f3a038053b108b76971e07eafd980c6b
jinjo <creationer625@outlook.com>
2024-05-26 14:09:34 +0800

    add photo.tsx update photoviewLine

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,13 +1,19 @@
 import Image from "next/image";
-import {Button} from "@radix-ui/themes"
+import { Button } from "@radix-ui/themes"
 import { Container } from "@/components/ui/Container";
 import Headline from "@/components/Headline";
-export default function Home() {
+import { getSettings } from '@/sanity/queries'
+import { Photos } from '@/components/Photos'
+const BlogHome = async () => {
+  const settings = await getSettings()
   return (
     <>
-    <Container className="mt-10">
+      <Container className="mt-10">
         <Headline />
       </Container>
+      {settings.heroPhotos && <Photos photos={settings.heroPhotos} />}
     </>
   );
 }
+
+export default BlogHome
