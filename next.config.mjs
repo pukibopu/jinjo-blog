84feeb30e4e3c146e749dd6bd934abe153a98276
jinjo <creationer625@outlook.com>
2024-05-26 14:08:56 +0800

    update nextconfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,44 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+!process.env.SKIP_ENV_VALIDATION && (await import('./env.mjs'))
 
-export default nextConfig;
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.sanity.io',
+        port: '',
+        pathname: `/images/${process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}/**`,
+      }
+    ],
+  },
+
+  experimental: {
+    taint: true,
+  },
+
+  redirects() {
+    return [
+      
+    ]
+  },
+
+  rewrites() {
+    return [
+      {
+        source: '/feed',
+        destination: '/feed.xml',
+      },
+      {
+        source: '/rss',
+        destination: '/feed.xml',
+      },
+      {
+        source: '/rss.xml',
+        destination: '/feed.xml',
+      },
+    ]
+  },
+}
+
+export default nextConfig
