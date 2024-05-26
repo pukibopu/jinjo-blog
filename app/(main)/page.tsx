ee003746d1e5cd47f3ca9d911c8e712201151930
jinjo <creationer625@outlook.com>
2024-05-26 13:36:53 +0800

    add and initialize sanity and prisma, add HeaderLine

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,9 +1,13 @@
 import Image from "next/image";
 import {Button} from "@radix-ui/themes"
+import { Container } from "@/components/ui/Container";
+import Headline from "@/components/Headline";
 export default function Home() {
   return (
-    <main className="">
-     <Button>123</Button>
-    </main>
+    <>
+    <Container className="mt-10">
+        <Headline />
+      </Container>
+    </>
   );
 }
